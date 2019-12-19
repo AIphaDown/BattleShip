3cfaa2af34475ea1e426f589ce597a91df66863e
AIphaDown <stoda2005@gmail.com>
2019-12-18 17:17:37 -0800

    added placement for ships, not finished

--- js/main.js
+++ js/main.js
@@ -1,17 +1,64 @@
+
 $(function() {
     var playerBoard = [];
+    var playerShips = [];
     var compBoard = [];
+    var compShips = [];
     var compGuessBoard = [];
     init();
 
-    function init()
+    function Ship(name, length, board)
     {
-    playerBoard = [];
-    compBoard = [];
-    compGuessBoard = [];
-    for (var i = 0; i < 10; i++)
-    {
+        this.name = name;
+        this.sunk = false;
+        this.squares = [];
+        this.board = board;
 
+        this.place = function()
+        {
+            var placed = false;
+            var vertical = Math.round(Math.random()) == 0;
+            While(!placed)
+            {
+                if(vertical)
+                {
+                    var c = Math.floor(Math.random * this.board.length);
+                    var r = Math.floor(Math.random * this.board.length - length);
+                    for (var i = 0; i < length; i++)
+                    {
+                        this.squares.push([r + i, c]);
+                        if(this.board[r + i][c] == "S")
+                        {
+                            this.squares = [];
+                            break;
+                        }
+                    }
+                } else
+                {
+                //left off here 12/18/19
+                }
+            }
+        }
     }
-    }
+
+    function init() {
+        playerBoard = [];
+        compBoard = [];
+        compGuessBoard = [];
+        for (var i = 0; i < 10; i++) {
+            var row = [];
+            for (var j = 0; i < 10; i++) {
+                row.push("W");
+                var id = i + "_" + j;
+                $("#player").append("<div id='"+id+"' class='gridsquare'></div>");
+                $("#computer").append("<div id='"+id+"' class='gridsquare'></div>");
+                }
+//           Add row to the player board
+                playerBoard.push(row);
+//           Add row to computer board
+                compBoard.push(row.slice());
+//           Add row to compGuessBoard board
+                compGuessBoard.push(row.slice());
+            }
+        }
 });
