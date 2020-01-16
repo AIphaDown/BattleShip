5dc21407fc696bb26a2fb2355e5d4ea5d2032002
AIphaDown <stoda2005@gmail.com>
2020-01-15 17:04:10 -0800

    hi

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 
-$(function() {
+$(function()
+{
     var playerBoard = [];
     var playerShips = [];
     var compBoard = [];
@@ -7,41 +8,52 @@ $(function() {
     var compGuessBoard = [];
     init();
 
-    function Ship(name, length, board){
+    function Ship(name, length, board)
+    {
         this.name = name;
         this.sunk = false;
         this.squares = [];
         this.board = board;
 
-        this.place = function() {
+        this.place = function()
+        {
            var placed = false;
            var vertical = Math.round(Math.random()) == 0;
-           while(!placed) {
-               if(vertical) {
+           while(!placed)
+           {
+               if(vertical)
+               {
                    var c = Math.floor(Math.random() * this.board.length);
                    var r = Math.floor(Math.random() * (this.board.length-length));
-                   for (var i=0; i < length; i++) {
+                   for (var i=0; i < length; i++)
+                   {
                        this.squares.push([r + i, c]);
-                       if(this.board[r+i][c] == "S") {
+                       if(this.board[r+i][c] == "S")
+                       {
                            this.squares = [];
                            break;
                        }
                    }
                }
-           } else {
+           }
+           else
+           {
                var r = Math.floor(Math.random() * this.board.length);
                var c = Math.floor(Math.random() * (this.board.length - length));
                for (var i = 0; i < length; i++) {
                    this.squares.push([r, c + i]);
-                   if (this.board[r][c + i] == "S") {
+                   if (this.board[r][c + i] == "S")
+                   {
                        this.squares = [];
                        break;
                    }
                }
            }
-           if(this.squares.length == length) {
+           if(this.squares.length == length)
+           {
                placed = true;
-               for (var i = 0; i < this.squares.length; i++) {
+               for (var i = 0; i < this.squares.length; i++)
+               {
                    var r = this.squares[i][0];
                    var c = this.squares[i][1];
                    this.board[r][c] = "S";
@@ -50,11 +62,14 @@ $(function() {
         }
       } /* End function Ship */
 
-    function drawBoard(board, player) {
+    function drawBoard(board, player)
+    {
         for (var i = 0; i < board.length; i++) {
-            for (var j = 0; j < board[i].length; j++) {
+            for (var j = 0; j < board[i].length; j++)
+            {
                 var color = "#1AD1FF";
-                if(board[i][j] == "S" && player === "player") {
+                if(board[i][j] == "S" && player === "player")
+                {
                     color = "gray";
                 }
                 $("#" + player + " > #" + i + "_" + j).css("background-color", color);
